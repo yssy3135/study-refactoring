2ecb1772c5412b842e7fc6d2a75f072907a32c7b
yssy3135 <ys3135@gmail.com>
2025-01-22 23:35:15 +0900

    [1장] amountFor method 변수 이름 명확하게 변경
    perf -> aPerformance

--- src/main/java/ch1/Example.java
+++ src/main/java/ch1/Example.java
@@ -36,22 +36,22 @@ public class Example {
     }
 
     // 함수 쪼개기
-    public static int amountFor(Performance perf, Play play) {
+    public static int amountFor(Performance aPerformance, Play play) {
         int result = 0;
 
         switch(play.type) {
             case "tragedy" :
                 result = 40000;
-                if(perf.getAudience() > 30) {
-                    result += 1000 * (perf.getAudience() -30);
+                if(aPerformance.getAudience() > 30) {
+                    result += 1000 * (aPerformance.getAudience() -30);
                 }
                 break;
             case "comedy" :
                 result = 30000;
-                if (perf.getAudience() > 20) {
-                    result += 10000 + 500 * (perf.getAudience() - 20);
+                if (aPerformance.getAudience() > 20) {
+                    result += 10000 + 500 * (aPerformance.getAudience() - 20);
                 }
-                result += 300 * perf.getAudience();
+                result += 300 * aPerformance.getAudience();
                 break;
             default:
                 throw new IllegalArgumentException("알 수 없는 장르 : " + play.getType());
