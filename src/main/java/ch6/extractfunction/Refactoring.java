46eb41241dabf22e2f9cef94209eb2d1e0bcfb25
yssy3135 <ys3135@gmail.com>
2025-03-10 00:07:09 +0900

    [6장] 세부 사항을 출력 추출

--- src/main/java/ch6/extractfunction/Refactoring.java
+++ src/main/java/ch6/extractfunction/Refactoring.java
@@ -7,9 +7,7 @@ public class Refactoring {
     public void printOwing(Invoice invoice) {
         int outstanding = 0;
 
-        System.out.println("*****************");
-        System.out.println("**** 고객 채무 ****");
-        System.out.println("*****************");
+        printBanner();
 
         // 미해결 채무 (outstanding) 을 계산한다.
         for (Order o : invoice.getOrders()) {
@@ -20,10 +18,20 @@ public class Refactoring {
         LocalDateTime today = Clock.today();
         invoice.dueDate = today.plusDays(30);
 
+        printDetails(invoice, outstanding);
+
+    }
+
+    private void printDetails(Invoice invoice, int outstanding) {
         // 세부 사항을 출력한다.
         System.out.println(String.format("고객명: %s", invoice.customer));
         System.out.println(String.format("채무액: %d", outstanding));
         System.out.println(String.format("마감일: %s", invoice.dueDate));
+    }
 
+    private void printBanner() {
+        System.out.println("*****************");
+        System.out.println("**** 고객 채무 ****");
+        System.out.println("*****************");
     }
 }
