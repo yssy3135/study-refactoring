350a3fb15454de903e9671f3a5adb7948437a05b
yssy3135 <ys3135@gmail.com>
2025-03-10 23:18:16 +0900

    [6장] 함수 선언 바꾸기, 매개변수 속성으로 바꾸기, 함수 선언 바꾸기 적용

--- src/main/java/ch6/changefunctiondeclation/RefactorCustomer.java
+++ src/main/java/ch6/changefunctiondeclation/RefactorCustomer.java
@@ -6,17 +6,12 @@ public class RefactorCustomer {
 
     Address address;
 
-
-    public boolean inNewEngland(RefactorCustomer aCustomer) {
-        return xxNewInNewEngland(this.address.state);
-    }
-
-    private static boolean xxNewInNewEngland(String stateCode) {
+    private static boolean inNewEngland(String stateCode) {
         return List.of("MA", "CT", "ME", "VT", "NH", "RI").contains(stateCode);
     }
 
     public List<RefactorCustomer> getEnglandCustomers(List<RefactorCustomer> customers) {
-        return customers.stream().filter(c -> c.xxNewInNewEngland(c.address.state)).toList();
+        return customers.stream().filter(c -> c.inNewEngland(c.address.state)).toList();
     }
 
 }
