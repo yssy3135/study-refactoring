914edd59cdb74a0337e9dc18b803d29354c07289
yssy3135 <ys3135@gmail.com>
2025-03-10 22:43:30 +0900

    [6장] 수량 할인 변수 추출

--- src/main/java/ch6/extractvariable/Refactoring.java
+++ src/main/java/ch6/extractvariable/Refactoring.java
@@ -5,8 +5,9 @@ public class Refactoring {
         // 가격(price) = 기본가격 - 수량 할인 + 배송비
         Integer basePrice = order.quantity * order.itemPrice;
 
+        double quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
         return basePrice -
-                Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
+                quantityDiscount +
                 Math.min(order.quantity * order.itemPrice * 0.1 , 100);
     }
 }
