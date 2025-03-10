756ddaf4d1d4510042a87ace05442ccf82a509f0
yssy3135 <ys3135@gmail.com>
2025-03-10 22:42:40 +0900

    [6장] 가격 계산 변수 추출

--- src/main/java/ch6/extractvariable/Refactoring.java
+++ src/main/java/ch6/extractvariable/Refactoring.java
@@ -3,7 +3,9 @@ package ch6.extractvariable;
 public class Refactoring {
     public double price(Order order) {
         // 가격(price) = 기본가격 - 수량 할인 + 배송비
-        return order.quantity * order.itemPrice -
+        Integer basePrice = order.quantity * order.itemPrice;
+
+        return basePrice -
                 Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
                 Math.min(order.quantity * order.itemPrice * 0.1 , 100);
     }
