f5d15b82a3075d97cefa37cb706a667169181f35
yssy3135 <ys3135@gmail.com>
2025-03-10 22:44:36 +0900

    [6장] 배송비 변수 추출

--- src/main/java/ch6/extractvariable/Refactoring.java
+++ src/main/java/ch6/extractvariable/Refactoring.java
@@ -6,8 +6,7 @@ public class Refactoring {
         Integer basePrice = order.quantity * order.itemPrice;
 
         double quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
-        return basePrice -
-                quantityDiscount +
-                Math.min(order.quantity * order.itemPrice * 0.1 , 100);
+        double shipping = Math.min(basePrice * 0.1, 100);
+        return basePrice - quantityDiscount + shipping;
     }
 }
