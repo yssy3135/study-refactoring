2696a9d55e03a4cb45d138c24b7b3cad4a3d9702
yssy3135 <ys3135@gmail.com>
2025-03-10 23:15:22 +0900

    [6장] 함수 선언 바꾸기, 매개변수 속성으로 바꾸기, 변수 추출

---
+++ src/main/java/ch6/changefunctiondeclation/RefactorCustomer.java
@@ -0,0 +1,21 @@
+package ch6.changefunctiondeclation;
+
+import java.util.List;
+
+public class RefactorCustomer {
+
+    Address address;
+
+
+    public boolean inNewEngland(RefactorCustomer aCustomer) {
+        String stateCode = this.address.state;
+        return List.of("MA", "CT", "ME", "VT", "NH", "RI").contains(stateCode);
+    }
+
+    public List<RefactorCustomer> getEnglandCustomers(List<RefactorCustomer> customers) {
+        return customers.stream().filter(c -> c.inNewEngland(c)).toList();
+    }
+
+}
+
+
