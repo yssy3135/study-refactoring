1620ffb0a5ce1b90a9677b1788bdca574c4d94d5
yssy3135 <ys3135@gmail.com>
2025-03-11 23:43:24 +0900

    [6장] 매개변수 객체 만들기, 함수 선언 변경

--- src/main/java/ch6/introduceparameterobject/Refactoring.java
+++ src/main/java/ch6/introduceparameterobject/Refactoring.java
@@ -1,4 +1,17 @@
 package ch6.introduceparameterobject;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Refactoring {
+
+    Station station;
+
+
+    public List<Reading> readingsOutsideRange(Station station, int min, int max, NumberRange range) {
+        return station.readings
+                .stream()
+                .filter(r -> r.temp < min || r.temp > max)
+                .collect(Collectors.toList());
+    }
 }
