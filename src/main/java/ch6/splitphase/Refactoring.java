a71d5a2e5656b23f9fcb9dc2f78ffc77af12f137
yssy3135 <ys3135@gmail.com>
2025-03-13 22:56:37 +0900

    [6장] 단계 쪼개기, price 데이터 구조로 basePrice 옮기기

--- src/main/java/ch6/splitphase/Refactoring.java
+++ src/main/java/ch6/splitphase/Refactoring.java
@@ -4,16 +4,18 @@ public class Refactoring {
     public double priceOrder(Product product, int quantity, ShippingMethod shippingMethod) {
         int basePrice = product.basePrice * quantity;
         int discount = Math.max(quantity - product.discountThreshold, 0) * product.basePrice * product.discountRate;
-        int price = applyShipping(basePrice, shippingMethod, quantity, discount);
+        PriceData priceData = new PriceData();
+        priceData.basePrice = basePrice;
+        int price = applyShipping(priceData, shippingMethod, quantity, discount);
         return price;
     }
 
-    private int applyShipping( int basePrice, ShippingMethod shippingMethod, int quantity, int discount) {
-        int shippingPerCase = basePrice > shippingMethod.discountThreshold ?
+    private int applyShipping(PriceData basePrice, ShippingMethod shippingMethod, int quantity, int discount) {
+        int shippingPerCase = basePrice.basePrice > shippingMethod.discountThreshold ?
                 shippingMethod.discountFee :
                 shippingMethod.feePerCase;
         int shippingCost = quantity * shippingPerCase;
-        int price = basePrice - discount * shippingCost;
+        int price = basePrice.basePrice - discount * shippingCost;
         return price;
     }
 }
