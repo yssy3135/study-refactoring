02f27c4cd431bd426ffccb62beaff147476b95de
yssy3135 <ys3135@gmail.com>
2025-03-13 22:55:09 +0900

    [6장] 단계 쪼개기, 배송비 계산 함수로 추출

--- src/main/java/ch6/splitphase/Refactoring.java
+++ src/main/java/ch6/splitphase/Refactoring.java
@@ -4,6 +4,11 @@ public class Refactoring {
     public double priceOrder(Product product, int quantity, ShippingMethod shippingMethod) {
         int basePrice = product.basePrice * quantity;
         int discount = Math.max(quantity - product.discountThreshold, 0) * product.basePrice * product.discountRate;
+        int price = applyShipping(basePrice, shippingMethod, quantity, discount);
+        return price;
+    }
+
+    private int applyShipping( int basePrice, ShippingMethod shippingMethod, int quantity, int discount) {
         int shippingPerCase = basePrice > shippingMethod.discountThreshold ?
                 shippingMethod.discountFee :
                 shippingMethod.feePerCase;
