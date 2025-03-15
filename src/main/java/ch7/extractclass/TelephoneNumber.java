0b4c55d4d3f03a82db669f7018c9d4a0aa1054af
yssy3135 <ys3135@gmail.com>
2025-03-15 19:15:33 +0900

    [7장] 클래스 추출하기, 예제 작성

---
+++ src/main/java/ch7/extractclass/TelephoneNumber.java
@@ -0,0 +1,22 @@
+package ch7.extractclass;
+
+public class TelephoneNumber {
+    String areaCode;
+    String number;
+
+    public String getAreaCode() {
+        return areaCode;
+    }
+
+    public void setAreaCode(String areaCode) {
+        this.areaCode = areaCode;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
