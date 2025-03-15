3b96a939e21d0364e880426d257ab27461a455ed
yssy3135 <ys3135@gmail.com>
2025-03-15 19:10:04 +0900

    [7장] 임시 변수를 질의 함수로 바꾸기, basePrice 인라인

--- src/main/java/ch7/replacetempwithquery/Order.java
+++ src/main/java/ch7/replacetempwithquery/Order.java
@@ -11,11 +11,11 @@ public class Order {
     }
 
    public double price() {
-       final Integer basePrice = basePrice();
+
        double discountFactor = 0.98;
 
-       if(basePrice > 1000) discountFactor -= 0.03;
-       return basePrice * discountFactor;
+       if(basePrice() > 1000) discountFactor -= 0.03;
+       return basePrice() * discountFactor;
    }
 
     private int basePrice() {
