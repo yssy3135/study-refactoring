6af6da3cc9b7e7c6de54a2218771a99bbfcdcde4
yssy3135 <ys3135@gmail.com>
2025-03-15 19:08:37 +0900

    [7장] 임시 변수를 질의 함수로 바꾸기, 예제 작성

---
+++ src/main/java/ch7/replacetempwithquery/Item.java
@@ -0,0 +1,10 @@
+package ch7.replacetempwithquery;
+
+public class Item {
+
+    int price;
+
+    public Item(int price) {
+        this.price = price;
+    }
+}
---
+++ src/main/java/ch7/replacetempwithquery/Order.java
@@ -0,0 +1,20 @@
+package ch7.replacetempwithquery;
+
+public class Order {
+
+    Integer quantity;
+    Item item;
+
+    public Order(int quantity, Item item) {
+        this.quantity = quantity;
+        this.item = item;
+    }
+
+   public double price() {
+       int basePrice = this.quantity * this.item.price;
+       double discountFactor = 0.98;
+
+       if(basePrice > 1000) discountFactor -= 0.03;
+       return basePrice * discountFactor;
+   }
+}
