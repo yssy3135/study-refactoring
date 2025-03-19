e94117ba76fafba5b84160eeabc9dd7a18328900
yssy3135 <ys3135@gmail.com>
2025-03-19 23:47:53 +0900

    [8장] 필드 이동하기, CustomerContract 필드와 접근자 추가

--- src/main/java/ch8/movefield/CustomerContract.java
+++ src/main/java/ch8/movefield/CustomerContract.java
@@ -4,10 +4,21 @@ import java.time.LocalDateTime;
 
 public class CustomerContract {
     LocalDateTime startDate;
+    double discountRate;
 
 
-    public CustomerContract(LocalDateTime startDate) {
+    public CustomerContract(LocalDateTime startDate, double discountRate) {
         this.startDate = startDate;
+        this.discountRate = discountRate;
+    }
+
+
+    public double getDiscountRate() {
+        return discountRate;
+    }
+
+    public void setDiscountRate(double discountRate) {
+        this.discountRate = discountRate;
     }
 
 
