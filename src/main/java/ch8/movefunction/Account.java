aff3e4ba5740f013ebd276241a9bcfc324327145
yssy3135 <ys3135@gmail.com>
2025-03-19 23:38:44 +0900

    [8장] 함수 이동하기 예제 작성, 옮겨진 함수 인라인

--- src/main/java/ch8/movefunction/Account.java
+++ src/main/java/ch8/movefunction/Account.java
@@ -7,22 +7,7 @@ public class Account {
 
     public double bankCharge() {
         double result = 4.5;
-        if (this.daysOverdrawn > 0) result += overdraftCharge();
+        if (this.daysOverdrawn > 0) result += this.type.overdraftCharge(daysOverdrawn);
         return result;
-    }
-
-    private double overdraftCharge() {
-        if (this.type.isPremium) {
-            int bankCharge = 10;
-            if (this.daysOverdrawn <= 7) {
-                return bankCharge;
-            }
-            else {
-                return bankCharge * (this.daysOverdrawn - 7) * 0.85;
-            }
-        }
-        else {
-            return this.daysOverdrawn * 1.75;
-        }
     }
 }
