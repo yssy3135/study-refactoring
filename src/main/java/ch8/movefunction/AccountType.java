2ef29501076025693a7d14559627c442ec851519
yssy3135 <ys3135@gmail.com>
2025-03-19 23:38:16 +0900

    [8장] 함수 이동하기 예제 작성, AccountType 클래스로 메서드 이동

--- src/main/java/ch8/movefunction/AccountType.java
+++ src/main/java/ch8/movefunction/AccountType.java
@@ -3,6 +3,19 @@ package ch8.movefunction;
 public class AccountType {
     protected boolean isPremium;
 
-
+    public double overdraftCharge(Integer daysOverdrawn) {
+        if (isPremium) {
+            int bankCharge = 10;
+            if (daysOverdrawn <= 7) {
+                return bankCharge;
+            }
+            else {
+                return bankCharge * (daysOverdrawn - 7) * 0.85;
+            }
+        }
+        else {
+            return daysOverdrawn * 1.75;
+        }
+    }
 }
 
