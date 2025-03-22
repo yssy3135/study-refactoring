960ceb0da6ee7add74b9bb4dca1fed5adf90aea4
yssy3135 <ys3135@gmail.com>
2025-03-22 23:56:42 +0900

    [8장] 반복문 쪼개기, 중복 코드 제거, 파이프라인으로 변경

--- src/main/java/ch8/splitloop/Main.java
+++ src/main/java/ch8/splitloop/Main.java
@@ -1,23 +1,25 @@
 package ch8.splitloop;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 public class Main {
 
 
-    public Integer calculateYoungestTotalSalary(People[] people) {
+    public String calculateYoungestTotalSalary(People[] people) {
+
+        return String.format("최연소 %s, 총 급여: %d", youngestAge(people), totalSalary(people));
+    }
+
+    private Integer totalSalary(People[] people) {
+        return Arrays.stream(people).map(p -> p.salary).reduce(0, Integer::sum);
+    }
+
+    private Integer youngestAge(People[] people) {
         int youngest = Optional.ofNullable(people[0]).map(p -> p.age).orElse(Integer.MAX_VALUE);
-        int totalSalary = 0;
         for (People person : people) {
-            totalSalary += person.salary;
+            youngest = Math.min(youngest, person.age);
         }
-
-        for (People person : people) {
-            if (person.age < youngest) {
-                youngest = person.age;
-            }
-        }
-
-        return totalSalary;
+        return youngest;
     }
 }
