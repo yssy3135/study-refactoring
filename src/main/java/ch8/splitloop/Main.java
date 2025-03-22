b188f5b727979cb9d54e91274ddb64dd463ff576
yssy3135 <ys3135@gmail.com>
2025-03-22 23:50:10 +0900

    [8장] 반복문 쪼개기, 예제 작성

---
+++ src/main/java/ch8/splitloop/Main.java
@@ -0,0 +1,19 @@
+package ch8.splitloop;
+
+import java.util.Optional;
+
+public class Main {
+
+
+    public Integer calculateYoungestTotalSalary(People[] people) {
+        int youngest = Optional.ofNullable(people[0]).map(p -> p.age).orElse(Integer.MAX_VALUE);
+        int totalSalary = 0;
+        for (People person : people) {
+            if (person.age < youngest) {
+                youngest = person.age;
+            }
+            totalSalary += person.salary;
+        }
+        return totalSalary;
+    }
+}
---
+++ src/main/java/ch8/splitloop/People.java
@@ -0,0 +1,6 @@
+package ch8.splitloop;
+
+public class People {
+    int salary;
+    int age;
+}
