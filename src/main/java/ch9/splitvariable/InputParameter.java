a67d09b604aae607cf1ffdc25328f09224a393a3
yssy3135 <ys3135@gmail.com>
2025-03-24 23:24:19 +0900

    [9장] 변수 쪼개기, inputValue 쪼개기

--- src/main/java/ch9/splitvariable/InputParameter.java
+++ src/main/java/ch9/splitvariable/InputParameter.java
@@ -1,7 +1,8 @@
 package ch9.splitvariable;
 
 public class InputParameter {
-    public int discount(int inputValue, int quantity) {
+    public int discount(int originalInputValue, int quantity) {
+        int inputValue = originalInputValue;
         if (inputValue > 50) inputValue -= 2;
         if (quantity > 100) inputValue -= 1;
         return inputValue;
