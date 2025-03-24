a34b80c30f63e9e3f8f2069b0669bf6a4312370c
yssy3135 <ys3135@gmail.com>
2025-03-24 23:48:04 +0900

    [9장] 파생 변수를 질의 함수로 바꾸기 , 변수 쪼개기 먼저 적용하여 해결

--- src/main/java/ch9/replacederivedvariablewithquery/Ex2ProductionPlan.java
+++ src/main/java/ch9/replacederivedvariablewithquery/Ex2ProductionPlan.java
@@ -4,15 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Ex2ProductionPlan {
-    int production;
+    int initialProduction;
+    int productionAccumulate;
     List<Adjustment> adjustments = new ArrayList<>();
 
-    public int getProduction() {
-        return this.production;
+    public Ex2ProductionPlan(int production) {
+        this.initialProduction = production;
+        this.productionAccumulate = 0;
+        adjustments = new ArrayList<>();
+
     }
 
-    public void applyAdjustment(Adjustment anAdjustment) {
-        adjustments.add(anAdjustment);
-        this.production += anAdjustment.amount;
+    public int getProduction() {
+        assert(this.productionAccumulate == this.calculatedProductionAccumulator());
+        return this.initialProduction + this.productionAccumulate;
+    }
+
+    public int calculatedProductionAccumulator() {
+        return adjustments.stream()
+                .reduce(0, (sum, a) -> sum + a.amount, Integer::sum);
     }
 }
