634c9286feaa290c1f35a2b149171ee9c51d86ab
yssy3135 <ys3135@gmail.com>
2025-03-24 23:17:17 +0900

    [9장] 변수 쪼개기, 새로운 이름 변수 할당, 불변

--- src/main/java/ch9/splitvariable/Haggis.java
+++ src/main/java/ch9/splitvariable/Haggis.java
@@ -3,14 +3,14 @@ package ch9.splitvariable;
 public class Haggis {
     public double distanceTravelled(Scenario scenario, int time) {
         double result;
-        int acc = scenario.primaryForce / scenario.mass; // 가속도 = 힘 * 질량
+        final int primaryAcceleration = scenario.primaryForce / scenario.mass; // 가속도 = 힘 * 질량
         int primaryTime = Math.min(time, scenario.delay);
-        result = 0.5 * acc * primaryTime * primaryTime; // 전파된 거리
+        result = 0.5 * primaryAcceleration * primaryTime * primaryTime; // 전파된 거리
         int secondaryTime = time - scenario.delay;
         if (secondaryTime > 0) {
             // 두 번째 힘을 반영해 다시 계산
-            int primaryVelocity = acc * scenario.delay;
-            acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
+            int primaryVelocity = primaryAcceleration * scenario.delay;
+            int acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
             result += primaryVelocity * secondaryTime + 0.5 * acc * secondaryTime * secondaryTime;
         }
         return result;
