43bc903d67709e7e0cbbec0002e45109a107e025
yssy3135 <ys3135@gmail.com>
2025-03-25 00:00:57 +0900

    [9장] 참조를 값으로 바꾸기, TelephoneNumber 불변으로 변경

--- src/main/java/ch9/changereferencetovalue/TelephoneNumber.java
+++ src/main/java/ch9/changereferencetovalue/TelephoneNumber.java
@@ -4,19 +4,17 @@ public class TelephoneNumber {
     String areaCode;
     String number;
 
-    public String getAreaCode() {
-        return areaCode;
+    public TelephoneNumber(String areaCode, String number) {
+        this.areaCode = areaCode;
+        this.number = number;
     }
 
-    public void setAreaCode(String areaCode) {
-        this.areaCode = areaCode;
+    public String getAreaCode() {
+        return areaCode;
     }
 
     public String getNumber() {
         return number;
     }
 
-    public void setNumber(String number) {
-        this.number = number;
-    }
 }
