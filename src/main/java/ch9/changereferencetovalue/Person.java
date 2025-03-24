b1d3f70479742fe10fb981bff094cd7898f292ca
yssy3135 <ys3135@gmail.com>
2025-03-25 00:03:25 +0900

    [9장] 참조를 값으로 바꾸기, TelephoneNumber 불변으로 변경

--- src/main/java/ch9/changereferencetovalue/Person.java
+++ src/main/java/ch9/changereferencetovalue/Person.java
@@ -3,13 +3,13 @@ package ch9.changereferencetovalue;
 public class Person {
     TelephoneNumber telephoneNumber;
 
-    public Person() {
-        telephoneNumber = new TelephoneNumber();
+    public Person(String areaCode, String number) {
+        telephoneNumber = new TelephoneNumber(areaCode, number);
     }
 
     public String getOfficeAreaCode() { return telephoneNumber.areaCode; }
-    public void setOfficeAreaCode(String areaCode) { telephoneNumber.setAreaCode(areaCode); }
+    public void setOfficeAreaCode(String areaCode) { new TelephoneNumber(areaCode, this.getOfficeNumber()); }
 
     public String getOfficeNumber() { return telephoneNumber.number; }
-    public void setOfficeNumber(String number ) { telephoneNumber.setNumber(number);}
+    public void setOfficeNumber(String number ) { new TelephoneNumber(this.getOfficeAreaCode(), number); }
 }
