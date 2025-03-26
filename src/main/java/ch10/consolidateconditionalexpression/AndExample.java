c359736eea0b03a253c279268c29d655d4a949bb
yssy3135 <ys3135@gmail.com>
2025-03-26 23:28:09 +0900

    [10장] 조건식 통합하기 , and 연산자 결합

--- src/main/java/ch10/consolidateconditionalexpression/AndExample.java
+++ src/main/java/ch10/consolidateconditionalexpression/AndExample.java
@@ -5,10 +5,9 @@ public class AndExample {
     private Employee anEmployee;
 
     public double disabilityAmount() {
-        if(anEmployee.onVacation) {
-            if(anEmployee.seniority > 10) {
-                return 1;
-            }
+        if((anEmployee.onVacation)
+            && (anEmployee.seniority > 10)) {
+            return 1;
         }
 
         return 0.5;
