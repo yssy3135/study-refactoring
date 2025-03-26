1e6ed1e11ba3606afb34fe539d7b5bc3dd9cb388
yssy3135 <ys3135@gmail.com>
2025-03-27 00:15:54 +0900

    [10장] 중첩 조건문을 보호 구문으로 바꾸기, 조건식 통합

--- src/main/java/ch10/replacenestedconditionalwithguardclauses/ReverseCondition.java
+++ src/main/java/ch10/replacenestedconditionalwithguardclauses/ReverseCondition.java
@@ -5,12 +5,12 @@ public class ReverseCondition {
 
     public double adjustedCapital(Instrument anInstrument) {
         double result = 0;
-        if(anInstrument.capital <= 0) {
+        if(anInstrument.capital <= 0
+        || anInstrument.interestRate <= 0
+        || anInstrument.duration <= 0) {
             return result;
         }
-        if(anInstrument.interestRate <= 0 && anInstrument.duration <= 0) {
-            return result;
-        }
+
         result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
         return result;
     }
