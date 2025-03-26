99aee91107c44fb3f60929be2fef24a46ca3a106
yssy3135 <ys3135@gmail.com>
2025-03-27 00:12:50 +0900

    [10장] 중첩 조건문을 보호 구문으로 바꾸기, 반대조건 예제 작성

---
+++ src/main/java/ch10/replacenestedconditionalwithguardclauses/Instrument.java
@@ -0,0 +1,9 @@
+package ch10.replacenestedconditionalwithguardclauses;
+
+public class Instrument {
+    public int capital;
+    public double interestRate;
+    public int duration;
+    public double income;
+    public double adjustmentFactor;
+}
---
+++ src/main/java/ch10/replacenestedconditionalwithguardclauses/ReverseCondition.java
@@ -0,0 +1,16 @@
+package ch10.replacenestedconditionalwithguardclauses;
+
+public class ReverseCondition {
+
+
+    public double adjustedCapital(Instrument anInstrument) {
+        double result = 0;
+        if(anInstrument.capital > 0) {
+            if(anInstrument.interestRate > 0 && anInstrument.duration > 0) {
+                result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
+            }
+        }
+        return result;
+    }
+
+}
