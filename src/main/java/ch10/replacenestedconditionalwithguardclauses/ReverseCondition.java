089f0fb78b2b5b7f1cb70e06056ffcf9117ef2b1
yssy3135 <ys3135@gmail.com>
2025-03-27 00:16:21 +0900

    [10장] 중첩 조건문을 보호 구문으로 바꾸기, result 변수 제거

--- src/main/java/ch10/replacenestedconditionalwithguardclauses/ReverseCondition.java
+++ src/main/java/ch10/replacenestedconditionalwithguardclauses/ReverseCondition.java
@@ -4,15 +4,14 @@ public class ReverseCondition {
 
 
     public double adjustedCapital(Instrument anInstrument) {
-        double result = 0;
+
         if(anInstrument.capital <= 0
         || anInstrument.interestRate <= 0
         || anInstrument.duration <= 0) {
-            return result;
+            return 0;
         }
 
-        result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
-        return result;
+        return (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
     }
 
 }
