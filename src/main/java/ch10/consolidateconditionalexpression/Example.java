614755220bf0c9a3f47dd9d64c593aa110fbbe8c
yssy3135 <ys3135@gmail.com>
2025-03-26 23:24:53 +0900

    [10장] 조건식 통합하기 , 순차적 검사 통합

--- src/main/java/ch10/consolidateconditionalexpression/Example.java
+++ src/main/java/ch10/consolidateconditionalexpression/Example.java
@@ -5,9 +5,9 @@ public class Example {
     private Employee anEmployee;
 
     public Integer disabilityAmount() {
-        if(anEmployee.seniority < 2) return 0;
-        if(anEmployee.monthsDisabled > 12) return 0;
-        if(anEmployee.isPartTime) return 0;
+        if((anEmployee.seniority < 2)
+                || (anEmployee.monthsDisabled > 12)
+                ||(anEmployee.isPartTime)) return 0;
 
         return 0;
     }
