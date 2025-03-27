889c8cf40418772c39c8d554da865c2d68f7360d
yssy3135 <ys3135@gmail.com>
2025-03-27 23:03:49 +0900

    [10장] 조건부 로직을 다형성으로 바꾸기, 메소드 클래스로 묶기

--- src/main/java/ch10/replaceconditionalwithpolymorphism/Bird.java
+++ src/main/java/ch10/replaceconditionalwithpolymorphism/Bird.java
@@ -6,4 +6,31 @@ public class Bird {
     public int numberOfCoconuts;
     public int voltage;
     public boolean isNailed;
+
+    public Bird(Bird bird) {
+        this.type = bird.type;
+        this.name = bird.name;
+        this.numberOfCoconuts = bird.numberOfCoconuts;
+        this.voltage = bird.voltage;
+        this.isNailed = bird.isNailed;
+    }
+
+    public String plumage() {
+        return switch (type) {
+            case "유럽 제비" -> "보통이다";
+            case "아프리카 제비" -> numberOfCoconuts > 2 ? "지쳤다" : "보통이다";
+            case "노르웨이 파랑 앵무" -> voltage > 100 ? "그을렸다" : "예쁘다";
+            default -> "알 수 없다";
+        };
+    }
+
+    public Integer airSpeedVelocity() {
+        return switch (type) {
+            case "유럽 제비" -> 35;
+            case "아프리카 제비" -> 40 - 2 * numberOfCoconuts;
+            case "노르웨이 파랑 앵무" -> (isNailed) ? 0 : 10 + voltage / 10;
+            default -> null;
+        };
+
+    }
 }
--- src/main/java/ch10/replaceconditionalwithpolymorphism/BirdInfo.java
+++ src/main/java/ch10/replaceconditionalwithpolymorphism/BirdInfo.java
@@ -11,24 +11,10 @@ public class BirdInfo {
     }
 
     public String plumage(Bird bird) {
-        switch (bird.type) {
-            case "유럽 제비":
-                return "보통이다";
-            case "아프리카 제비":
-                return bird.numberOfCoconuts > 2 ? "지쳤다" : "보통이다";
-            case "노르웨이 파랑 앵무":
-                return bird.voltage > 100 ? "그을렸다" : "예쁘다";
-            default:
-                return "알 수 없다";
-        }
+        return new Bird(bird).plumage();
     }
 
     public Integer airSpeedVelocity(Bird bird) {
-        return switch (bird.type) {
-            case "유럽 제비" -> 35;
-            case "아프리카 제비" -> 40 - 2 * bird.numberOfCoconuts;
-            case "노르웨이 파랑 앵무" -> (bird.isNailed) ? 0 : 10 + bird.voltage / 10;
-            default -> null;
-        };
+        return new Bird(bird).airSpeedVelocity();
     }
 }
