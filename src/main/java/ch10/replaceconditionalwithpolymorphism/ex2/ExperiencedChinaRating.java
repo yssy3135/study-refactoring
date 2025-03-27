346e4e7c30d08d73e3204616e8f6cabbe4c715f5
yssy3135 <ys3135@gmail.com>
2025-03-27 23:32:18 +0900

    [10장] 조건부 로직을 다형성으로 바꾸기- 변형동작, 변형 동작을 담을 빈 서브클래스 만들기

---
+++ src/main/java/ch10/replaceconditionalwithpolymorphism/ex2/ExperiencedChinaRating.java
@@ -0,0 +1,11 @@
+package ch10.replaceconditionalwithpolymorphism.ex2;
+
+public class ExperiencedChinaRating extends Rating {
+
+    public ExperiencedChinaRating(Voyage voyage, History history) {
+        super(voyage, history);
+    }
+
+
+
+}
