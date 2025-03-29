a1b2127c45e22e83b85ddaeb18370c40e1eb7813
yssy3135 <ys3135@gmail.com>
2025-03-29 18:19:44 +0900

    [11장] 질의 함수와 변경함수 분리하기, 예제 작성

---
+++ src/main/java/ch11/separatequeryfrommodifier/Example.java
@@ -0,0 +1,24 @@
+package ch11.separatequeryfrommodifier;
+
+import java.util.List;
+
+public class Example {
+
+    public String alertForMiscreant(List<String> people) {
+        for (int i = 0; i < people.size(); i++) {
+            if (people.get(i).equals("조커")) {
+                setOffAlarms();
+                return "조커";
+            }
+            if (people.get(i).equals("사루만")) {
+                setOffAlarms();
+                return "사루만";
+            }
+        }
+        return "";
+    }
+
+    private void setOffAlarms() {
+
+    }
+}
