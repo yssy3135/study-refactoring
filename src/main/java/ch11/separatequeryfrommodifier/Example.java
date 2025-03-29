70e622e9f28433957e71ae95b2ce3e988b3d8b77
yssy3135 <ys3135@gmail.com>
2025-03-29 18:20:27 +0900

    [11장] 질의 함수와 변경함수 분리하기, 복제하여 지르이 목적에 맞게 이름 변경

--- src/main/java/ch11/separatequeryfrommodifier/Example.java
+++ src/main/java/ch11/separatequeryfrommodifier/Example.java
@@ -18,6 +18,20 @@ public class Example {
         return "";
     }
 
+    public String findMiscreant(List<String> people) {
+        for (int i = 0; i < people.size(); i++) {
+            if (people.get(i).equals("조커")) {
+                setOffAlarms();
+                return "조커";
+            }
+            if (people.get(i).equals("사루만")) {
+                setOffAlarms();
+                return "사루만";
+            }
+        }
+        return "";
+    }
+
     private void setOffAlarms() {
 
     }
