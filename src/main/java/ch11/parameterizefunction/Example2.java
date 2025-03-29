dd77d5077d6310f29e2f872b1a551e53bd48e0a0
yssy3135 <ys3135@gmail.com>
2025-03-29 18:36:55 +0900

    [11장] 함수 매개변수화 하기, 다른 함수도 새로 만든 매개변수화 함수 호출하도록 변경

--- src/main/java/ch11/parameterizefunction/Example2.java
+++ src/main/java/ch11/parameterizefunction/Example2.java
@@ -6,23 +6,11 @@ public class Example2 {
     public Double baseCharge(Integer usage) {
         if(usage < 0) return usd(0.0);
 
-        Double amount = bottomBand(usage) * 0.03
+        Double amount = withinBand(usage, 0, 100) * 0.03
                 + withinBand(usage, 100, 200) * 0.05
-                + topBand(usage) * 0.07;
+                + withinBand(usage, 200, Integer.MAX_VALUE) * 0.07;
 
         return usd(amount);
-    }
-
-    private Double bottomBand(Integer usage) {
-        return (double) Math.min(usage, 100);
-    }
-
-    private Double middleBand(Integer usage) {
-        return (double) (usage > 100 ? Math.min(usage, 200) - 100 : 0);
-    }
-
-    private Double topBand(Integer usage) {
-        return (double) (usage > 200 ? usage - 200 : 0);
     }
 
     private Double usd(Double amount) {
