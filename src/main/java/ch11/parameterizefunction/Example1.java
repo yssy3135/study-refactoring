9dceb713c6fdab81ed3acc5d3b9739d79f5dcb04
yssy3135 <ys3135@gmail.com>
2025-03-29 18:29:30 +0900

    [11장] 함수 매개변수화 하기, 메서드 매개변수로 전환

--- src/main/java/ch11/parameterizefunction/Example1.java
+++ src/main/java/ch11/parameterizefunction/Example1.java
@@ -1,6 +1,7 @@
 package ch11.parameterizefunction;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 
 public class Example1 {
 
@@ -10,6 +11,10 @@ public class Example1 {
 
     public void fivePercentRaise(Person aPerson) {
         aPerson.salary = aPerson.salary.multiply(BigDecimal.valueOf(1.05));
+    }
+
+    public void raise(Person aPerson, BigDecimal factor) {
+        aPerson.salary = aPerson.salary.multiply((factor.add(BigDecimal.ONE)));
     }
 
 }
