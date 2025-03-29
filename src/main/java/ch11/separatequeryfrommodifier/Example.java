89fee574ef8641e523460affdd7cd3881cfa1211
yssy3135 <ys3135@gmail.com>
2025-03-29 18:22:01 +0900

    [11장] 질의 함수와 변경함수 분리하기, 지르이 관련 코드 제거

--- src/main/java/ch11/separatequeryfrommodifier/Example.java
+++ src/main/java/ch11/separatequeryfrommodifier/Example.java
@@ -4,18 +4,15 @@ import java.util.List;
 
 public class Example {
 
-    public String alertForMiscreant(List<String> people) {
+    public void alertForMiscreant(List<String> people) {
         for (int i = 0; i < people.size(); i++) {
             if (people.get(i).equals("조커")) {
                 setOffAlarms();
-                return "조커";
             }
             if (people.get(i).equals("사루만")) {
                 setOffAlarms();
-                return "사루만";
             }
         }
-        return "";
     }
 
     public String findMiscreant(List<String> people) {
