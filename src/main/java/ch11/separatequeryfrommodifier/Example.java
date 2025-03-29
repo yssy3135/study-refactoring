004b7035844f6e053a551df6824aaabe5108f587
yssy3135 <ys3135@gmail.com>
2025-03-29 18:20:42 +0900

    [11장] 질의 함수와 변경함수 분리하기, 복제함수 부수효과 제거

--- src/main/java/ch11/separatequeryfrommodifier/Example.java
+++ src/main/java/ch11/separatequeryfrommodifier/Example.java
@@ -21,11 +21,9 @@ public class Example {
     public String findMiscreant(List<String> people) {
         for (int i = 0; i < people.size(); i++) {
             if (people.get(i).equals("조커")) {
-                setOffAlarms();
                 return "조커";
             }
             if (people.get(i).equals("사루만")) {
-                setOffAlarms();
                 return "사루만";
             }
         }
