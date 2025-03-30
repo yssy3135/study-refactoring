5c6684486f208154f0b59e1f0eb6ec7a00132e30
yssy3135 <ys3135@gmail.com>
2025-03-30 21:13:03 +0900

    [11장] 객체 통째로 넘기기, 함수 옮기기 수행

--- src/main/java/ch11/preservewholeobject/HeatingPlan.java
+++ src/main/java/ch11/preservewholeobject/HeatingPlan.java
@@ -1,5 +1,7 @@
 package ch11.preservewholeobject;
 
+import ch10.decomposeconditional.Plan;
+
 public class HeatingPlan {
 
     TempRange daysTempRange;
@@ -7,5 +9,15 @@ public class HeatingPlan {
     public boolean withinRange(Integer bottom, Integer top) {
 
         return (bottom >= daysTempRange.low) && (top <= daysTempRange.high);
+    }
+
+    public boolean xxNewWithinRange(HeatingPlan plan, TempRange tempRange) {
+
+        Integer low = tempRange.low;
+        Integer high = tempRange.high;
+
+        boolean isWithInRange = plan.withinRange(low, high);
+
+        return isWithInRange;
     }
 }
