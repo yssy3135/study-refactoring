f32e9e2cbefd0a92569fb6ce51bf15ba7d085ae1
yssy3135 <ys3135@gmail.com>
2025-03-30 21:11:35 +0900

    [11장] 객체 통째로 넘기기, 메서드 호출 변수 추출 및 매개 변수 추출

--- src/main/java/ch11/preservewholeobject/Example2.java
+++ src/main/java/ch11/preservewholeobject/Example2.java
@@ -7,10 +7,12 @@ public class Example2 {
 
 
     public void caller(Room aRoom, HeatingPlan aPlan, List<String> alerts) {
-        Integer low = aRoom.daysTempRange.low;
-        Integer high = aRoom.daysTempRange.high;
+        TempRange tempRange = aRoom.daysTempRange;
+        Integer low = tempRange.low;
+        Integer high = tempRange.high;
 
-        if(aPlan.withinRange(low, high)) {
+        boolean isWithInRange = aPlan.withinRange(low, high);
+        if(!isWithInRange) {
             alerts.add("방 온도가 지정 범위를 벗어났습니다.");
         }
 
