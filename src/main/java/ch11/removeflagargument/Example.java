ec16011bbee9e4bd4ee6b9624e2d234ca82e5c65
yssy3135 <ys3135@gmail.com>
2025-03-30 18:28:58 +0900

    [11장] 플래그 인수 제거하기, 플래그에 따라 조건문 분해

--- src/main/java/ch11/removeflagargument/Example.java
+++ src/main/java/ch11/removeflagargument/Example.java
@@ -10,27 +10,35 @@ public class Example {
 
     public LocalDate deliveryDate(Order anOrder, Boolean isRush) {
         if (isRush) {
-            int deliveryTime;
-            if (List.of("MA", "CT").contains(anOrder.deliveryState)) {
-                deliveryTime = 1;
-            } else if (List.of("NY", "NH").contains(anOrder.deliveryState)) {
-                deliveryTime = 2;
-            } else {
-                deliveryTime = 3;
-            }
-            return anOrder.placedOn.plusDays(1 + anOrder.deliveryTime).toLocalDate();
+            return rushDeliveryDate(anOrder);
         } else {
-            int deliveryTime;
-            if (List.of("MA", "CT", "NY").contains(anOrder.deliveryState)) {
-                deliveryTime = 2;
-            } else if (List.of("ME", "NH").contains(anOrder.deliveryState)) {
-                deliveryTime = 3;
-            } else {
-                deliveryTime = 4;
-            }
-
-            return anOrder.placedOn.plusDays(2 + anOrder.deliveryTime).toLocalDate();
+            return regularDeliveryDate(anOrder);
         }
+    }
+
+    private static LocalDate regularDeliveryDate(Order anOrder) {
+        int deliveryTime;
+        if (List.of("MA", "CT", "NY").contains(anOrder.deliveryState)) {
+            deliveryTime = 2;
+        } else if (List.of("ME", "NH").contains(anOrder.deliveryState)) {
+            deliveryTime = 3;
+        } else {
+            deliveryTime = 4;
+        }
+
+        return anOrder.placedOn.plusDays(2 + anOrder.deliveryTime).toLocalDate();
+    }
+
+    private static LocalDate rushDeliveryDate(Order anOrder) {
+        int deliveryTime;
+        if (List.of("MA", "CT").contains(anOrder.deliveryState)) {
+            deliveryTime = 1;
+        } else if (List.of("NY", "NH").contains(anOrder.deliveryState)) {
+            deliveryTime = 2;
+        } else {
+            deliveryTime = 3;
+        }
+        return anOrder.placedOn.plusDays(1 + anOrder.deliveryTime).toLocalDate();
     }
 }
 
