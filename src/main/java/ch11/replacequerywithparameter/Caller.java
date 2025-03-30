70357bc8534b685acc856ecc37e316812e7485ee
yssy3135 <ys3135@gmail.com>
2025-03-30 22:18:03 +0900

    [11장] 질의 함수를 매개변수로 바꾸기, caller 클래스 추가 작성

---
+++ src/main/java/ch11/replacequerywithparameter/Caller.java
@@ -0,0 +1,26 @@
+package ch11.replacequerywithparameter;
+
+public class Caller {
+
+    public void call(HeatingPlan thePlan, Thermostat theThermostat) {
+
+        if(thePlan.targetTemperature() > theThermostat.currentTemperature) {
+            setToHeat();
+        }
+        else if(thePlan.targetTemperature() < theThermostat.currentTemperature) {
+            setToCool();
+        }
+        else setOff();
+    }
+
+    private void setOff() {
+
+    }
+
+    private void setToCool() {
+    }
+
+    private void setToHeat() {
+
+    }
+}
