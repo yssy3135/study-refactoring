c747083dcda7d9a64fb642fcc18d83e57dcb7d14
yssy3135 <ys3135@gmail.com>
2025-03-30 21:23:49 +0900

    [11장] 매개변수를 질의함수로 바꾸기, 참조 코드 모두 함수 호출로 변경

--- src/main/java/ch11/replaceparameterwithquery/Order.java
+++ src/main/java/ch11/replaceparameterwithquery/Order.java
@@ -1,18 +1,20 @@
 package ch11.replaceparameterwithquery;
 
 public class Order {
-    public double getPrice(int quantity, double itemPrice) {
+
+    private int quantity;
+
+    public double getPrice(double itemPrice) {
         double basePrice = quantity * itemPrice;
-        int discountLevel = getDiscountLevel(quantity);
-        return discountedPrice(basePrice, discountLevel);
+        return discountedPrice(basePrice);
     }
 
-    private static int getDiscountLevel(int quantity) {
+    private int getDiscountLevel() {
         return quantity > 100 ? 2 : 1;
     }
 
-    private double discountedPrice(double basePrice, int discountLevel) {
-        return switch (discountLevel) {
+    private double discountedPrice(double basePrice) {
+        return switch (getDiscountLevel()) {
             case 1 -> basePrice * 0.95;
             case 2 -> basePrice * 0.9;
             default -> throw new IllegalArgumentException("no such discount level");
