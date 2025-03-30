9f3791003ec5c57797ab81cebb8d44b41e8c8b73
yssy3135 <ys3135@gmail.com>
2025-03-30 18:28:13 +0900

    [11장] 플래그 인수 제거하기, 예제 작성

---
+++ src/main/java/ch11/removeflagargument/Example.java
@@ -0,0 +1,37 @@
+package ch11.removeflagargument;
+
+import java.sql.Time;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Example {
+
+
+    public LocalDate deliveryDate(Order anOrder, Boolean isRush) {
+        if (isRush) {
+            int deliveryTime;
+            if (List.of("MA", "CT").contains(anOrder.deliveryState)) {
+                deliveryTime = 1;
+            } else if (List.of("NY", "NH").contains(anOrder.deliveryState)) {
+                deliveryTime = 2;
+            } else {
+                deliveryTime = 3;
+            }
+            return anOrder.placedOn.plusDays(1 + anOrder.deliveryTime).toLocalDate();
+        } else {
+            int deliveryTime;
+            if (List.of("MA", "CT", "NY").contains(anOrder.deliveryState)) {
+                deliveryTime = 2;
+            } else if (List.of("ME", "NH").contains(anOrder.deliveryState)) {
+                deliveryTime = 3;
+            } else {
+                deliveryTime = 4;
+            }
+
+            return anOrder.placedOn.plusDays(2 + anOrder.deliveryTime).toLocalDate();
+        }
+    }
+}
+
+
---
+++ src/main/java/ch11/removeflagargument/Order.java
@@ -0,0 +1,10 @@
+package ch11.removeflagargument;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class Order {
+    public String deliveryState;
+    public LocalDateTime placedOn;
+    public Integer deliveryTime;
+}
