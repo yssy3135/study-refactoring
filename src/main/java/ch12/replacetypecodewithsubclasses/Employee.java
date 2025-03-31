a23e85c18f7a99a2b06a88468ca2f742d168c152
yssy3135 <ys3135@gmail.com>
2025-03-31 23:45:31 +0900

    [12장] 타입 코드를 서브클래스로 바꾸기. 검증로직 제거

--- src/main/java/ch12/replacetypecodewithsubclasses/Employee.java
+++ src/main/java/ch12/replacetypecodewithsubclasses/Employee.java
@@ -7,16 +7,10 @@ public class Employee {
     String name;
 
     public Employee(String name, String type) {
-        this.validateType(type);
+
         this.name = name;
     }
 
-    private void validateType(String type) {
-
-        if(!List.of("salesperson", "engineer", "manager").contains(type)) {
-            throw new IllegalArgumentException("%s 라는 직원 유형은 없습니다.".formatted(type));
-        };
-    }
 
     public String toString() {
         return "name: %s, type: %s".formatted(this.name);
