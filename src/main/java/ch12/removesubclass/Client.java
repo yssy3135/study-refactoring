47d7737d54f369a0b781d7d7814d74d18fd8c6f4
yssy3135 <ys3135@gmail.com>
2025-04-02 23:56:58 +0900

    [12장] 서브클래스 제거하기 , 클라이언트 리팩터링

--- src/main/java/ch12/removesubclass/Client.java
+++ src/main/java/ch12/removesubclass/Client.java
@@ -1,5 +1,6 @@
 package ch12.removesubclass;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Client {
@@ -7,5 +8,17 @@ public class Client {
 
     public void call(List<Person> people) {
         long numberOfMales = people.stream().filter(p -> p instanceof Male).count();
+    }
+
+    public List<Person> loadFromInput(List<Person> data) {
+        return data.stream().map(Client::createPerson).toList();
+    }
+
+    private static Person createPerson(Person aRecord) {
+        return switch (aRecord.getGenderCode()) {
+            case "M" -> new Male(aRecord.getName());
+            case "F" -> new Female(aRecord.getName());
+            default -> new Person(aRecord.getName());
+        };
     }
 }
